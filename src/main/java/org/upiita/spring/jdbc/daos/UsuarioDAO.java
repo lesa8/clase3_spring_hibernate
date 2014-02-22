21aa526b36cb6f9b77e9f74c62cb8db3055e8e3b
lesa8 <yolotzing_00@hotmail.com>
2014-02-22 12:32:58 -0600

    Cambio muy importante

--- src/main/java/org/upiita/spring/jdbc/daos/UsuarioDAO.java
+++ src/main/java/org/upiita/spring/jdbc/daos/UsuarioDAO.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.upiita.spring.jdbc.entidades.Usuario;
 
+//Comentario Para probar en el repositorio git
+
 public interface UsuarioDAO {
 
 	public Usuario buscaUsuarioPorId(Integer usuarioId);
